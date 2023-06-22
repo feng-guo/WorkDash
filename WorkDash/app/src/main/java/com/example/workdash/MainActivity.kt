71e88f86b5fcfe87d6250eb222f4f00357968540
Felix-Suen <felix.suen2000@gmail.com>
2023-06-22 15:41:46 -0400

    Experiment with Register and Login Page, using Firebase for authentication

--- WorkDash/app/src/main/java/com/example/workdash/MainActivity.kt
+++ WorkDash/app/src/main/java/com/example/workdash/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.workdash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val register = findViewById<Button>(R.id.register)
+        val login = findViewById<Button>(R.id.login)
+
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
