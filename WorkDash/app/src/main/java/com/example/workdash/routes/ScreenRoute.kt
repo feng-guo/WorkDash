015d71f2c2d0fc967d7f59cf571539b38d5e15ba
Feng <feng.guo2001@gmail.com>
2023-07-10 10:46:55 -0400

    bug fixes

--- WorkDash/app/src/main/java/com/example/workdash/routes/ScreenRoute.kt
+++ WorkDash/app/src/main/java/com/example/workdash/routes/ScreenRoute.kt
@@ -16,14 +16,14 @@ sealed class ScreenRoute(val route: String){
     object CurrentJobPostsEmployer: ScreenRoute(route = "current_job_posts_employer_screen")
     object JobDetailsEmployer: ScreenRoute(route = "job_details_employer_screen/{$JOB_ID_ARG$LOCATION_ID_ARG}") {
         fun passJobIdAndLocationId(jobId: String, locationId: String): String {
-            return "job_details_worker_screen/$jobId$locationId"
+            return "job_details_employer_screen/$jobId$locationId"
         }
     }
     object ChooseLocationEmployer: ScreenRoute(route = "choose_location_employer_screen")
     object AddLocationEmployer: ScreenRoute(route = "add_location_employer_screen")
     object AddPostEmployer: ScreenRoute(route = "add_post_employer_screen/{$LOCATION_ID_ARG}") {
         fun passLocationId(locationId: String): String {
-            return "job_details_worker_screen/$locationId"
+            return "add_post_employer_screen/$locationId"
         }
     }
     object SignUpEmployer: ScreenRoute(route = "sign_up_employer_screen")
