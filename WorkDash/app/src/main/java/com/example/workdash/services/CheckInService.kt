e70e895cfcde3c9e668fccf1a4cfa2c7e0e9dd37
Eric <z242jian@uwaterloo.ca>
2023-07-20 17:31:05 -0400

    update Eric's own timelog

--- WorkDash/app/src/main/java/com/example/workdash/services/CheckInService.kt
+++ WorkDash/app/src/main/java/com/example/workdash/services/CheckInService.kt
@@ -62,7 +62,7 @@ object CheckInService {
 
     }
 
-    fun getJobModelByJobId(jobId: String, onSuccess: (JobModel?) -> Unit, onError: (String) -> Unit) {
+    private fun getJobModelByJobId(jobId: String, onSuccess: (JobModel?) -> Unit, onError: (String) -> Unit) {
         val reference = dbInt.getReference("jobs").child(jobId)
 
         reference.addValueEventListener(object : ValueEventListener {
@@ -96,6 +96,91 @@ object CheckInService {
         })
     }
 
+
+    
+    fun getJobDetailsByJobIdAndEmployeeId(jobId: String, employeeId: String){
+        findMatchedJobByJobIdAndEmployeeId(jobId,employeeId,
+            onSuccess = { matchedJob ->
+                // Process the matched job data
+            },
+            onError = { errorMessage ->
+                // Handle the error
+                Log.e("GET MATCHED JOB ERROR", errorMessage)
+            }
+        )
+
+
+
+
+    }
+    private fun findMatchedJobByJobIdAndEmployeeId(
+        jobId: String,
+        employeeId: String,
+        onSuccess: (matchedJobModel?) -> Unit,
+        onError: (String) -> Unit
+    ) {
+        val reference = dbInt.getReference("matchedJob")
+
+        val query = reference.orderByChild("jobId_employeeId").equalTo("$jobId"+ "_" + "$employeeId")
+
+        query.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var matchedJob: matchedJobModel? = null
+                for (snapshot in dataSnapshot.children) {
+                    val job = snapshot.getValue(matchedJobModel::class.java)
+                    if (job?.jobId == jobId && job.employeeId == employeeId) {
+                        matchedJob = job
+                        break
+                    }
+                }
+                onSuccess(matchedJob)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                onError("Failed to find matched job: ${error.message}")
+            }
+        })
+    }
+
+
+    fun updateCheckInStateAndTime(jobId: String, employeeId: String, checkInState: String, checkInTime: String) {
+        val reference = dbInt.getReference("matchedJob")
+
+        // Set up the query to find the matchedJob with the provided jobId and employeeId
+        val query = reference.orderByChild("jobId_employeeId").equalTo("$jobId"+ "_" + "$employeeId")
+
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (snapshot in dataSnapshot.children) {
+                    val matchedJob = snapshot.getValue(matchedJobModel::class.java)
+                    matchedJob?.let {
+                        // Update the matchedJob model with the new CheckInState and CheckInTime
+                        it.checkInState = checkInState
+                        it.checkInTime = checkInTime
+
+                        // Update the matchedJob in Firebase
+                        val updates = mutableMapOf<String, Any>()
+                        updates[snapshot.key.toString()] = it
+
+                        reference.updateChildren(updates)
+                            .addOnSuccessListener {
+                                // Update successful
+                                // You can perform any action here after the update is successful
+                            }
+                            .addOnFailureListener { error ->
+                                // Handle the error
+                                Log.e("UPDATE MATCHED JOB MODEL ERROR", "Failed to update CheckInState and CheckInTime: ${error.message}")
+                            }
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle the error
+                Log.e("UPDATE MATCHED JOB MODEL ERROR", "Failed to retrieve matched job data: ${error.message}")
+            }
+        })
+    }
 
 
 }
