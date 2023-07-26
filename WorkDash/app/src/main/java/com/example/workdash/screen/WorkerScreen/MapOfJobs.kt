0a7c5aa05f4bcb2644d2a0d81c73104bc2e681bc
Feng <feng.guo2001@gmail.com>
2023-07-25 21:13:26 -0400

    Added top bar to map

--- WorkDash/app/src/main/java/com/example/workdash/screen/WorkerScreen/MapOfJobs.kt
+++ WorkDash/app/src/main/java/com/example/workdash/screen/WorkerScreen/MapOfJobs.kt
@@ -1,7 +1,16 @@
 package com.example.workdash.screen.WorkerScreen
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -16,6 +25,7 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MapOfJobs(
     navController: NavController,
@@ -23,26 +33,52 @@ fun MapOfJobs(
     val navBackStackEntry = navController.currentBackStackEntry
     val locationId = navBackStackEntry?.arguments?.getString(LOCATION_ID_ARG) ?: ""
     val locationViewModel = LocationViewModel()
-    val default = LatLng(43.4720424, -80.5262535)
+    val default = LatLng(43.4720424, -80.5371821)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(default, 17f)
+        position = CameraPosition.fromLatLngZoom(default, 15f)
     }
     val coordinates = remember {locationViewModel.getCoordinateList()}
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colorScheme.primary,
+                title = {
+                    Text("Job Map")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.popBackStack()
+                    }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
             coordinates.forEach { coordinate ->
                 Marker(
-                    state = rememberMarkerState(position = LatLng(coordinate.latitude, coordinate.longitude)),
+                    state = rememberMarkerState(
+                        position = LatLng(
+                            coordinate.latitude,
+                            coordinate.longitude
+                        )
+                    ),
                     title = locationViewModel.getLocation(coordinate.locationId).locationName,
                     snippet = locationViewModel.getLocation(coordinate.locationId).address.address,
                     onInfoWindowLongClick = {
                         navController.navigate(
-                            route = ScreenRoute.JobDetailsWorker.passJobIdAndLocationId(locationViewModel.getJobFromLocation(coordinate.locationId).jobId, coordinate.locationId)
+                            route = ScreenRoute.JobDetailsWorker.passJobIdAndLocationId(
+                                locationViewModel.getJobFromLocation(coordinate.locationId).jobId,
+                                coordinate.locationId
+                            )
                         )
                     }
                 )
             }
         }
+    }
 }
